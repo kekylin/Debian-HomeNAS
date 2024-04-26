4a766d0adb8637fbcd9fb62943edf111ec3aee6d
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-26 19:41:54 +0800

    Update install_cockpit.sh

--- install_cockpit.sh
+++ install_cockpit.sh
@@ -3,12 +3,12 @@
 # 1. 安装Cockpit
 # 启用向后移植存储库
 . /etc/os-release
-echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" > /etc/apt/sources.list.d/backports.list
-apt update
+echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
+sudo apt update
 # 安装或更新软件包
-apt install -t ${VERSION_CODENAME}-backports cockpit -y
+sudo apt install -t ${VERSION_CODENAME}-backports cockpit -y
 # 安装cockpit-pcp
-apt install -t ${VERSION_CODENAME}-backports cockpit-pcp -y
+sudo apt install -t ${VERSION_CODENAME}-backports cockpit-pcp -y
 
 # 2. 安装Cockpit附属组件
 # 配置45Drives Repo安装脚本
@@ -21,21 +21,21 @@ sudo apt install cockpit-navigator cockpit-file-sharing cockpit-identities -y
 # 询问用户是否需要安装虚拟机组件
 read -p "是否安装虚拟机组件？(y/n): " vm_install
 if [[ $vm_install == "y" ]]; then
-    apt install -t ${VERSION_CODENAME}-backports cockpit-machines -y
+    sudo apt install -t ${VERSION_CODENAME}-backports cockpit-machines -y
 fi
 
 # 询问用户是否需要安装Podman容器组件
 read -p "是否安装Podman容器组件？(y/n): " podman_install
 if [[ $podman_install == "y" ]]; then
-    apt install -t ${VERSION_CODENAME}-backports cockpit-podman -y
+    sudo apt install -t ${VERSION_CODENAME}-backports cockpit-podman -y
 fi
 
 # 询问用户是否需要安装Cockpit ZFS管理器
 read -p "是否安装Cockpit ZFS管理器？(y/n): " zfs_install
 if [[ $zfs_install == "y" ]]; then
     # 安装ZFS依赖
-    apt update
-    apt install -y zfs-dkms zfsutils-linux
+    sudo apt update
+    sudo apt install -y zfs-dkms zfsutils-linux
     # 克隆cockpit-zfs-manager仓库并复制到/usr/share/cockpit目录
     git clone https://github.com/optimans/cockpit-zfs-manager.git
     sudo cp -r cockpit-zfs-manager/zfs /usr/share/cockpit
