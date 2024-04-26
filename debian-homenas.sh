4d41251cdd1c8df8324902a552a282a90b5c203d
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-26 20:14:17 +0800

    Update debian-homenas.sh

--- debian-homenas.sh
+++ debian-homenas.sh
@@ -2,11 +2,11 @@
 
 # 安装必备软件
 apt update
-apt install -y sudo curl git vim wget
-apt install -y apt-transport-https ca-certificates
+apt install -y sudo curl git vim wget apt-transport-https ca-certificates
+
 # 添加第一个创建的用户至sudo组
 user_name=$(getent passwd | awk -F: '$3>=1000{print $1}' | head -n 1)
-usermod -aG sudo $user_name
+usermod -aG sudo "$user_name"
 
 # 更换国内镜像源
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
