91f1fe98f550cbd71f678bb8cb8d55d2cf483a80
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-26 21:03:06 +0800

    Update install_cockpit.sh

--- install_cockpit.sh
+++ install_cockpit.sh
@@ -10,20 +10,34 @@ apt update
 apt install -y -t $(. /etc/os-release && echo $VERSION_CODENAME)-backports cockpit cockpit-pcp
 apt install -y cockpit-navigator cockpit-file-sharing cockpit-identities
 
-# 安装官方组件
-components=("cockpit-machines" "cockpit-podman")
-for component in "${components[@]}"; do
-    read -p "是否安装${component}组件？(y/n): " install_component
-    if [[ $install_component == "y" ]]; then
-        apt install -y -t $(. /etc/os-release && echo $VERSION_CODENAME)-backports $component
-    fi
-done
+# 询问是否安装cockpit-machines
+read -p "是否安装cockpit-machines组件？(y/n): " install_machines
+if [[ $install_machines == "y" ]]; then
+    to_install+=("cockpit-machines")
+fi
+
+# 询问是否安装cockpit-podman
+read -p "是否安装cockpit-podman组件？(y/n): " install_podman
+if [[ $install_podman == "y" ]]; then
+    to_install+=("cockpit-podman")
+fi
 
 # 询问是否安装Cockpit ZFS管理器
 read -p "是否安装Cockpit ZFS管理器？(y/n): " install_zfs_manager
 if [[ $install_zfs_manager == "y" ]]; then
-    apt update
-    apt install -y zfs-dkms zfsutils-linux git
-    git clone https://github.com/optimans/cockpit-zfs-manager.git
-    cp -r cockpit-zfs-manager/zfs /usr/share/cockpit
+    to_install+=("Cockpit ZFS管理器")
 fi
+
+# 根据用户回答安装组件
+for component in "${to_install[@]}"; do
+    if [[ "$component" != "Cockpit ZFS管理器" ]]; then
+        apt install -y -t $(. /etc/os-release && echo $VERSION_CODENAME)-backports "$component"
+    else
+        if [[ $install_zfs_manager == "y" ]]; then
+            apt update
+            apt install -y zfs-dkms zfsutils-linux git
+            git clone https://github.com/optimans/cockpit-zfs-manager.git
+            cp -r cockpit-zfs-manager/zfs /usr/share/cockpit
+        fi
+    fi
+done
