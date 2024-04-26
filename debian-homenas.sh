cc1c1fc45e0d46115623e574bddaa8de16ca832c
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-26 19:17:15 +0800

    Update debian-homenas.sh

--- debian-homenas.sh
+++ debian-homenas.sh
@@ -10,33 +10,11 @@ usermod -aG sudo $user_name
 
 # 更换国内镜像源
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
-
-# 检测当前Debian系统的发行版本
-debian_version=$(lsb_release -cs)
-
-# 根据不同的发行版本选择相应的源
-case $debian_version in
-    "bookworm")
-        mirror="https://mirrors.tuna.tsinghua.edu.cn/debian/"
-        ;;
-    "bullseye")
-        mirror="https://mirrors.tuna.tsinghua.edu.cn/debian/"
-        ;;
-    "buster")
-        mirror="https://mirrors.tuna.tsinghua.edu.cn/debian/"
-        ;;
-    *)
-        echo "Unsupported Debian version."
-        exit 1
-        ;;
-esac
-
-# 替换软件源配置文件内容
 cat << EOF > /etc/apt/sources.list
-deb $mirror $debian_version main contrib non-free
-deb $mirror $debian_version-updates main contrib non-free
-deb $mirror $debian_version-backports main contrib non-free
-# 使用默认的安全源
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
 EOF
 
 apt update
