3bd2a965f88235b9956a8b77c97eed87fc7e7717
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-27 20:15:51 +0800

    Create systemsetup.sh

---
+++ systemsetup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 安装必备软件
+apt update
+apt install -y sudo curl git vim wget gnupg apt-transport-https ca-certificates
+
+# 添加第一个创建的用户至sudo组
+first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
+usermod -aG sudo "$first_user"
+
+# 更换国内镜像源
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cat << EOF > /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
+EOF
+
+# 更新系统
+apt update && apt upgrade -y
