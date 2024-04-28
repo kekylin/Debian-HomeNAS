7ca16e2940819ac815fe6c6134e871fbc57c68ff
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-28 18:34:14 +0800

    Update ServerManager.sh

--- ServerManager.sh
+++ ServerManager.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
+# ANSI 转义码颜色定义
+green='\033[0;32m'
+red='\033[0;31m'
+reset='\033[0m'
+
+# 函数：检查服务是否运行
+check_service() {
+    local service_name=$1
+    local service_status=$(systemctl is-active $service_name.service 2>/dev/null)
+    echo "$service_status"
+}
+
+# 函数：输出服务状态信息
+print_service_status() {
+    local service_name=$1
+    local service_url=$2
+    local service_status=$3
+
+    if [ "$service_status" == "active" ]; then
+        echo -e "${green}$service_name 服务已运行！${reset}"
+        echo -e "请通过浏览器访问 $service_url"
+    else
+        echo -e "${red}$service_name 服务未能正常运行，请检查。${reset}"
+    fi
+}
+
 # 获取主机的 IP 地址
 host_ip=$(hostname -I | awk '{print $1}')
 
-# 检查 Cockpit Web 服务是否正在运行
-cockpit_running=$(systemctl is-active cockpit.service >/dev/null 2>&1 && echo "true" || echo "false")
+# Cockpit Web 服务
+cockpit_status=$(check_service cockpit)
+print_service_status "Cockpit Web" "https://$host_ip:9090" "$cockpit_status"
 
-if [ "$cockpit_running" == "true" ]; then
-    echo "Cockpit Web 服务正在运行！"
-    echo "请访问 https://$host_ip:9090"
-else
-    echo "Cockpit Web 服务未能正常运行，请检查。"
+# Docker 服务
+docker_status=$(check_service docker)
+if [ "$docker_status" != "active" ]; then
+    echo -e "${red}Docker 服务未能正常运行，请检查。${reset}"
 fi
 
-# 检查 Docker 服务是否正在运行
-docker_running=$(systemctl is-active docker.service >/dev/null 2>&1 && echo "true" || echo "false")
-
-# 检查 Portainer 服务是否正在运行
+# Portainer 服务
 portainer_running=$(docker ps -q -f "name=portainer" | wc -l)
-
-if [ "$docker_running" == "true" ] && [ "$portainer_running" -gt 0 ]; then
-    echo "Docker Portainer 服务正在运行！"
-    echo "请访问 https://$host_ip:9443"
-elif [ "$docker_running" == "false" ]; then
-    echo "Docker 服务未能正常运行，请检查。"
-elif [ "$portainer_running" -eq 0 ]; then
-    echo "Portainer 服务未能正常运行，请检查。"
+if [ "$docker_status" == "active" ] && [ "$portainer_running" -gt 0 ]; then
+    print_service_status "Portainer" "https://$host_ip:9443" "active"
+elif [ "$docker_status" == "active" ] && [ "$portainer_running" -eq 0 ]; then
+    echo -e "${red}Portainer 服务未能正常运行，请检查。${reset}"
 fi
