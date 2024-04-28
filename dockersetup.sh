463a295aad3ee5060f82cdf3989c13866ec8a11c
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-28 08:20:24 +0800

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -20,6 +20,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 docker volume create portainer_data
 
 # 下载和安装 Portainer Server 容器
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name portainer --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
   portainer/portainer-ee:latest
