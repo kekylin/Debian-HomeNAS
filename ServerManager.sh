58d50d99ba4c8939f2b0a6e52eb372ed329946ff
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-28 18:00:15 +0800

    Update ServerManager.sh

--- ServerManager.sh
+++ ServerManager.sh
@@ -8,17 +8,22 @@ cockpit_running=$(systemctl is-active cockpit.service >/dev/null 2>&1 && echo "t
 
 if [ "$cockpit_running" == "true" ]; then
     echo "Cockpit Web 服务正在运行！"
-    echo "请访问 https://$host_ip:9090 来管理服务器。"
+    echo "请访问 https://$host_ip:9090"
 else
-    echo "Cockpit Web 服务未能正常运行，请检查安装或运行时的问题。"
+    echo "Cockpit Web 服务未能正常运行，请检查。"
 fi
 
-# 检查 Docker 是否正在运行
-docker_running=$(docker info >/dev/null 2>&1 && echo "true" || echo "false")
+# 检查 Docker 服务是否正在运行
+docker_running=$(systemctl is-active docker.service >/dev/null 2>&1 && echo "true" || echo "false")
 
-if [ "$docker_running" == "true" ]; then
-    echo "Docker 已经成功运行！"
-    echo "请访问 http://$host_ip:9443 来管理 Docker 服务。"
-else
-    echo "Docker 未能正常运行，请检查安装或运行时的问题。"
+# 检查 Portainer 服务是否正在运行
+portainer_running=$(docker ps -q -f "name=portainer" | wc -l)
+
+if [ "$docker_running" == "true" ] && [ "$portainer_running" -gt 0 ]; then
+    echo "Docker Portainer 服务正在运行！"
+    echo "请访问 http://$host_ip:9443"
+elif [ "$docker_running" == "false" ]; then
+    echo "Docker 服务未能正常运行，请检查。"
+elif [ "$portainer_running" -eq 0 ]; then
+    echo "Portainer 服务未能正常运行，请检查。"
 fi
