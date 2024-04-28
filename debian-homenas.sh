1a23541c3efcd807718aab668edb0dfc392dec73
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-28 17:51:17 +0800

    Update debian-homenas.sh

--- debian-homenas.sh
+++ debian-homenas.sh
@@ -5,6 +5,7 @@ wget -O systemsetup.sh -q --show-progress https://mirror.ghproxy.com/https://raw
 wget -O install_cockpit.sh -q --show-progress https://mirror.ghproxy.com/https://raw.githubusercontent.com/kekylin/debian-homenas/main/install_cockpit.sh
 wget -O systemsec.sh -q --show-progress https://mirror.ghproxy.com/https://raw.githubusercontent.com/kekylin/debian-homenas/main/systemsec.sh
 wget -O dockersetup.sh -q --show-progress https://mirror.ghproxy.com/https://raw.githubusercontent.com/kekylin/debian-homenas/main/dockersetup.sh
+wget -O dockersetup.sh -q --show-progress https://mirror.ghproxy.com/https://raw.githubusercontent.com/kekylin/debian-homenas/main/ServerManager.sh
 
 # 确保所有脚本都下载成功
 if [ $? -eq 0 ]; then
@@ -15,6 +16,7 @@ if [ $? -eq 0 ]; then
     bash install_cockpit.sh
     bash systemsec.sh
     bash dockersetup.sh
+    bash ServerManager.sh
 else
     echo "下载脚本失败，请检查网络连接或稍后再试。"
     exit 1
