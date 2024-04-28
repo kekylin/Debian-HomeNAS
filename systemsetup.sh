dd83f052c7ddf191474949016d86bc3b899aa686
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-28 17:17:22 +0800

    Update systemsetup.sh

--- systemsetup.sh
+++ systemsetup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# 安装必备软件
-apt update
-apt install -y sudo curl git vim wget gnupg apt-transport-https ca-certificates
-
-# 添加第一个创建的用户至sudo组
-first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
-usermod -aG sudo "$first_user"
-
 # 更换国内镜像源
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cat << EOF > /etc/apt/sources.list
@@ -19,3 +11,10 @@ EOF
 
 # 更新系统
 apt update && apt upgrade -y
+
+# 安装必备软件
+apt install -y sudo curl git vim wget gnupg apt-transport-https ca-certificates
+
+# 添加第一个创建的用户至sudo组
+first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
+usermod -aG sudo "$first_user"
