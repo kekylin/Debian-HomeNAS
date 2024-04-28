bec3e605e80da98a0c22f0679c37d863e63d4f99
kekylin <65521882+kekylin@users.noreply.github.com>
2024-04-28 18:01:30 +0800

    Update ServerManager.sh

--- ServerManager.sh
+++ ServerManager.sh
@@ -21,7 +21,7 @@ portainer_running=$(docker ps -q -f "name=portainer" | wc -l)
 
 if [ "$docker_running" == "true" ] && [ "$portainer_running" -gt 0 ]; then
     echo "Docker Portainer 服务正在运行！"
-    echo "请访问 http://$host_ip:9443"
+    echo "请访问 https://$host_ip:9443"
 elif [ "$docker_running" == "false" ]; then
     echo "Docker 服务未能正常运行，请检查。"
 elif [ "$portainer_running" -eq 0 ]; then
