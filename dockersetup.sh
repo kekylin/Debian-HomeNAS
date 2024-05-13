0d50b0025c98b0d2030e9f208410254ebd8d8955
kekylin <65521882+kekylin@users.noreply.github.com>
2024-05-13 18:32:12 +0800

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -16,7 +16,6 @@ sudo apt-get update
 # 安装 Docker Engine、containerd 和 Docker Compose
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
-#部署Docker管理工具Portainer。
 # 检查是否已经部署了同名容器
 check_container_existence() {
     local container_name="$1"
@@ -28,11 +27,41 @@ check_container_existence() {
     fi
 }
 
-# 检查是否已经部署了同名容器
+# 部署Docker管理工具Portainer。
 if ! check_container_existence "portainer"; then
     # 创建 Portainer Server 将使用的数据卷
-    docker volume create portainer_data
+    docker volume create portainer_data 2>/dev/null
 
     # 下载和安装 Portainer Server 容器
     docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+fi
+
+# 部署Docker管理工具 Dockge
+if ! check_container_existence "dockge"; then
+    # 创建 stacks 和 dockge 目录
+    sudo mkdir -p /opt/stacks /opt/dockge
+
+    # 进入 Dockge 目录
+    cd /opt/dockge || exit
+
+    # 创建 Docker Compose 文件
+    echo \
+    "version: \"3.8\"
+services:
+  dockge:
+    image: louislam/dockge:latest
+    container_name: dockge
+    restart: unless-stopped
+    ports:
+      - 5001:5001
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - ./data:/app/data
+      - /opt/stacks:/opt/stacks
+    environment:
+      - DOCKGE_STACKS_DIR=/opt/stacks" | \
+    sudo tee docker-compose.yml > /dev/null
+
+    # 运行部署命令
+    sudo docker compose up -d
 fi
