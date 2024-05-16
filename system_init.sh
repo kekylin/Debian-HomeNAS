adb28448104ab89017783aaee6174ce50e6bcd50
kekylin <65521882+kekylin@users.noreply.github.com>
2024-05-16 20:12:39 +0800

    Update system_init.sh

--- system_init.sh
+++ system_init.sh
@@ -14,7 +14,7 @@ EOF
 apt update && apt upgrade -y
 
 # 安装必备软件
-apt install -y sudo curl git vim wget gnupg apt-transport-https ca-certificates
+apt install -y sudo curl git vim wget exim4 gnupg apt-transport-https ca-certificates
 
 # 添加第一个创建的用户（ID：1000）至sudo组
 first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
