e75dcdb18d685bf017cb85ffa49c8894fd443fdf
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-07 20:32:49 +0800

    Update install_cockpit.sh

--- Shell/install_cockpit.sh
+++ Shell/install_cockpit.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
 # 启用向后移植存储库
-echo "deb http://deb.debian.org/debian $(. /etc/os-release && echo $VERSION_CODENAME)-backports main" > /etc/apt/sources.list.d/backports.list
+os_codename=$(awk -F= '/VERSION_CODENAME/{print $2}' /etc/os-release)
+echo "deb http://deb.debian.org/debian $os_codename-backports main" > /etc/apt/sources.list.d/backports.list
+
 # 配置45Drives Repo安装脚本（用于安装Navigator、File Sharing、Identities组件）
 curl -sSL https://repo.45drives.com/setup | bash
 apt update
 
 # 安装Cockpit及其附属组件（Navigator、File Sharing、Identities组件）
-apt install -y -t $(. /etc/os-release && echo $VERSION_CODENAME)-backports cockpit cockpit-pcp
-apt install -y cockpit-navigator cockpit-file-sharing cockpit-identities
+apt install -y -t "$os_codename-backports" cockpit cockpit-pcp cockpit-navigator cockpit-file-sharing cockpit-identities
 
 # 询问是否安装cockpit-machines
 read -p "是否安装虚拟机组件？(y/n): " install_machines
 
 if [[ $install_machines == "y" ]]; then
     # 安装 cockpit-machines 组件
-    apt install -y -t $(. /etc/os-release && echo $VERSION_CODENAME)-backports cockpit-machines
+    apt install -y -t "$os_codename-backports" cockpit-machines
 
     # 开启IP包转发功能
     echo "开启IP包转发功能..."
-
-    # 编辑 /etc/sysctl.conf 文件
     sysctl_conf="/etc/sysctl.conf"
     if grep -qE "^#?net.ipv4.ip_forward=1" "$sysctl_conf"; then
         sed -i 's/^#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' "$sysctl_conf"
     else
         echo "net.ipv4.ip_forward=1" >> "$sysctl_conf"
     fi
-
-    # 应用更改
     sysctl -p
-
     echo "已安装虚拟机组件。"
 else
     echo "已跳过虚拟机组件安装。"
 fi
 
 # 配置首页展示信息
-tee /etc/motd > /dev/null <<EOF
+cat <<EOF > /etc/motd
 我们信任您已经从系统管理员那里了解了日常注意事项。总结起来无外乎这三点：
 1、尊重别人的隐私。
 2、输入前要先考虑(后果和风险)。
@@ -45,98 +41,78 @@ tee /etc/motd > /dev/null <<EOF
 EOF
 
 # 安装Tuned系统调优工具
-apt install tuned -y
+apt install -y tuned
 
-# cockpit调优，设置自动注销闲置及Nginx反向代理Cockpit操作。
-# 检查/etc/cockpit/cockpit.conf配置文件是否存在，不存在则创建
-if [ ! -f "/etc/cockpit/cockpit.conf" ]; then
-    touch /etc/cockpit/cockpit.conf
+# Cockpit调优，设置自动注销闲置及Nginx反向代理Cockpit操作
+cockpit_conf="/etc/cockpit/cockpit.conf"
+if [[ ! -f "$cockpit_conf" ]]; then
+    mkdir -p /etc/cockpit
+    cat <<EOF > "$cockpit_conf"
+[Session]
+IdleTimeout=15
+Banner=/etc/cockpit/issue.cockpit
 
-    # 插入初始配置内容
-    echo "[Session]" > /etc/cockpit/cockpit.conf
-    echo "IdleTimeout=15" >> /etc/cockpit/cockpit.conf
-    echo "Banner=/etc/cockpit/issue.cockpit" >> /etc/cockpit/cockpit.conf
-
-    echo -e "\n[WebService]" >> /etc/cockpit/cockpit.conf
-    echo "ProtocolHeader = X-Forwarded-Proto" >> /etc/cockpit/cockpit.conf
-    echo "ForwardedForHeader = X-Forwarded-For" >> /etc/cockpit/cockpit.conf
-    echo "LoginTo = false" >> /etc/cockpit/cockpit.conf
-    echo "LoginTitle = HomeNAS" >> /etc/cockpit/cockpit.conf
+[WebService]
+ProtocolHeader = X-Forwarded-Proto
+ForwardedForHeader = X-Forwarded-For
+LoginTo = false
+LoginTitle = HomeNAS
+EOF
 fi
 
-# 检查/etc/cockpit/issue.cockpit配置文件是否存在，不存在则创建
-if [ ! -f "/etc/cockpit/issue.cockpit" ]; then
-    echo "基于Debian搭建HomeNAS！" > /etc/cockpit/issue.cockpit
-fi
+# 检查 /etc/cockpit/issue.cockpit 配置文件是否存在，不存在则创建
+issue_file="/etc/cockpit/issue.cockpit"
+[[ ! -f "$issue_file" ]] && echo "基于Debian搭建HomeNAS！" > "$issue_file"
 
 # 检查是否需要设置Cockpit外网访问
 read -p "是否设置Cockpit外网访问？(y/n): " response
-config_file="/etc/cockpit/cockpit.conf"
 
-# Function to delete configuration parameters
+# 删除配置参数函数
 delete_params() {
-    sed -i '/Origins/d' "$config_file"
-    sed -i '/Access-Control-Allow-Origin/d' "$config_file"
+    sed -i '/Origins/d' "$cockpit_conf"
+    sed -i '/Access-Control-Allow-Origin/d' "$cockpit_conf"
     echo "已删除Cockpit外网访问相关配置。"
 }
 
-# Function to set configuration parameters
+# 设置配置参数函数
 set_params() {
-    # 提取当前主机的内网IP地址段
-    internal_ip=$(hostname -I | awk '{print $1}')
-    internal_subnet=$(echo "$internal_ip" | sed -E 's/\.[0-9]+$/\.0\/24/')
-
-    # 配置Cockpit的Origins和Access-Control-Allow-Origin参数
-    if grep -q "Origins" "$config_file"; then
-        sed -i "s#^Origins = .*#Origins = https://$domain wss://$domain#" "$config_file"
+    internal_subnet=$(hostname -I | awk '{print $1}' | sed -E 's/\.[0-9]+$/\.0\/24/')
+    if grep -q "Origins" "$cockpit_conf"; then
+        sed -i "s#^Origins = .*#Origins = https://$domain wss://$domain#" "$cockpit_conf"
     else
-        sed -i "/\[WebService\]/a Origins = https://$domain wss://$domain" "$config_file"
+        sed -i "/\[WebService\]/a Origins = https://$domain wss://$domain" "$cockpit_conf"
     fi
-
-    if grep -q "Access-Control-Allow-Origin" "$config_file"; then
-        sed -i "s#^Access-Control-Allow-Origin: .*#Access-Control-Allow-Origin: https://$internal_subnet#" "$config_file"
+    if grep -q "Access-Control-Allow-Origin" "$cockpit_conf"; then
+        sed -i "s#^Access-Control-Allow-Origin: .*#Access-Control-Allow-Origin: https://$internal_subnet#" "$cockpit_conf"
     else
-        sed -i "/Origins = /a Access-Control-Allow-Origin: https://$internal_subnet" "$config_file"
+        sed -i "/Origins = /a Access-Control-Allow-Origin: https://$internal_subnet" "$cockpit_conf"
     fi
     echo "已设置Cockpit外网访问域名：https://$domain"
     echo "已设置内网访问地址段：https://$internal_subnet"
 }
 
-# 判断用户是否选择设置Cockpit外网访问
 if [[ -z "$response" || "$response" == "n" ]]; then
-    # 用户不做回应或者回答n，删除配置参数
-    [[ -f "$config_file" ]] && delete_params
+    [[ -f "$cockpit_conf" ]] && delete_params
     echo "已跳过Cockpit外网访问配置。"
 else
-    # 提示用户输入外网访问域名
     read -p "请输入Cockpit外网访问域名和端口号（例如 example.com:9090）： " domain
-    # 移除输入中的协议部分
     domain=$(echo "$domain" | sed -E 's#^https?://##')
-    # 设置配置参数
-    [[ ! -f "$config_file" ]] && echo "[WebService]" > "$config_file"
+    [[ ! -f "$cockpit_conf" ]] && echo "[WebService]" > "$cockpit_conf"
     set_params
 fi
 
 echo "Cockpit调优配置完成。"
 
 # 设置Cockpit接管网络配置（网络管理工具由network改为NetworkManager）
-setup_network_configuration() {
-    local interfaces_file="/etc/network/interfaces"
-    
-    if [ -f "$interfaces_file" ]; then
-        # 注释掉未注释的行
-        sed -i '/^[^#].*/ s/^/#/' "$interfaces_file"
-    else
-        echo "文件 '$interfaces_file' 不存在，跳过操作。"
-    fi
-}
+interfaces_file="/etc/network/interfaces"
+if [[ -f "$interfaces_file" ]]; then
+    sed -i '/^[^#]/ s/^/#/' "$interfaces_file"
+else
+    echo "文件 '$interfaces_file' 不存在，跳过操作。"
+fi
+
 # 重启Network Manager服务
-restart_network_manager() {
-    systemctl restart NetworkManager && echo "已重启 Network Manager 服务。"
-}
-# 执行主程序
-setup_network_configuration
-restart_network_manager
+systemctl restart NetworkManager && echo "已重启 Network Manager 服务。"
 
 # 重启cockpit服务
 systemctl try-restart cockpit
