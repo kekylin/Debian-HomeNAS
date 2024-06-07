700f95ad6ca2ead53aa6d80daec46f43b1fda3b5
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-07 22:09:26 +0800

    Update install_docker.sh

--- Shell/install_docker.sh
+++ Shell/install_docker.sh
@@ -13,7 +13,7 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 # 更新索引文件并安装 Docker 相关组件
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # 添加第一个创建的用户（ID：1000）至docker组
 first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
