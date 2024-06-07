74942b3238371690ed22b1f25321dbefe06dcf82
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-07 20:46:44 +0800

    Update email_config.sh

--- Shell/email_config.sh
+++ Shell/email_config.sh
@@ -4,23 +4,13 @@
 read -p "是否需要配置邮件发送服务？(y/n): " choice
 
 # 如果用户回答不是 y，则跳过配置邮件发送服务操作
-if [ "$choice" != "y" ]; then
+if [[ "$choice" != "y" ]]; then
     echo "已跳过配置邮件发送服务。"
     exit 0
 fi
 
-# 备份配置文件
-sudo cp /etc/exim4/update-exim4.conf.conf /etc/exim4/update-exim4.conf.conf.backup
-sudo cp /etc/exim4/passwd.client /etc/exim4/passwd.client.backup
-sudo cp /etc/email-addresses /etc/email-addresses.backup
-
-# 清空配置文件内容
-sudo truncate -s 0 /etc/exim4/update-exim4.conf.conf
-sudo truncate -s 0 /etc/exim4/passwd.client
-sudo truncate -s 0 /etc/email-addresses
-
-# 插入内容到/etc/exim4/update-exim4.conf.conf文件中
-cat <<EOF | sudo tee -a /etc/exim4/update-exim4.conf.conf
+# 插入内容到 /etc/exim4/update-exim4.conf.conf 文件中
+cat <<EOF > /etc/exim4/update-exim4.conf.conf
 dc_eximconfig_configtype='satellite'
 dc_other_hostnames=''
 dc_local_interfaces='127.0.0.1 ; ::1'
@@ -41,19 +31,21 @@ read -p "请输入QQ邮箱账户: " qq_account
 read -sp "请输入QQ邮箱授权密码: " qq_password
 echo
 
-# 编辑/etc/exim4/passwd.client文件配置邮件发送账户
-echo "qq-smtp.l.qq.com:$qq_account:$qq_password" | sudo tee -a /etc/exim4/passwd.client
-echo "*.qq.com:$qq_account:$qq_password" | sudo tee -a /etc/exim4/passwd.client
-echo "smtp.qq.com:$qq_account:$qq_password" | sudo tee -a /etc/exim4/passwd.client
+# 编辑 /etc/exim4/passwd.client 文件配置邮件发送账户
+cat <<EOF > /etc/exim4/passwd.client
+qq-smtp.l.qq.com:$qq_account:$qq_password
+*.qq.com:$qq_account:$qq_password
+smtp.qq.com:$qq_account:$qq_password
+EOF
 
-# 编辑/etc/email-addresses文件，添加邮箱地址
-echo "root: $qq_account" | sudo tee -a /etc/email-addresses
+# 编辑 /etc/email-addresses 文件，添加邮箱地址
+echo "root: $qq_account" > /etc/email-addresses
 
 # 重启exim4服务
-sudo service exim4 restart
+service exim4 restart
 
 # 检测exim4服务是否正常运行
-if sudo service exim4 status | grep -q "active (running)"; then
+if service exim4 status | grep -q "active (running)"; then
     echo -e "\e[32m邮件发送服务已配置完成。\e[0m"
 else
     echo -e "\e[31m邮件发送服务运行异常，请检查服务状态。\e[0m"
