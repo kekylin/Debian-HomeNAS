38d014840bb1e40049897d0ee62f330ccdae42b0
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-07 22:05:28 +0800

    Update install_docker.sh

--- Shell/install_docker.sh
+++ Shell/install_docker.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# 使用华为镜像源安装Docker
+# 使用清华大学镜像源安装Docker
 # 安装依赖
-apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-# 运行公钥
-curl -fsSL https://mirrors.huaweicloud.com/docker-ce/linux/debian/gpg | apt-key add -
-# 添加软件仓库
-add-apt-repository "deb [arch=amd64] https://mirrors.huaweicloud.com/docker-ce/linux/debian $(lsb_release -cs) stable"
+apt-get install ca-certificates curl gnupg
+# 信任 Docker 的 GPG 公钥并添加仓库
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://gitee.com/kekylin/docker-ce/raw/master/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 # 更新索引文件并安装 Docker 相关组件
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # 添加第一个创建的用户（ID：1000）至docker组
 first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
