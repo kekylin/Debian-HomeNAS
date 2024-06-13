523f6b49257b67b5535435b5585e80f050403267
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-13 19:23:13 +0800

    Update system_init.sh

--- Shell/system_init.sh
+++ Shell/system_init.sh
@@ -4,10 +4,10 @@
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 # 更换国内镜像源
 cat << EOF > /etc/apt/sources.list
-deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
-deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
-deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
-deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
+deb https://mirrors.huaweicloud.com/debian/ bookworm main contrib non-free non-free-firmware
+deb https://mirrors.huaweicloud.com/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb https://mirrors.huaweicloud.com/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb https://mirrors.huaweicloud.com/debian-security bookworm-security main contrib non-free non-free-firmware
 EOF
 
 # 更新系统
