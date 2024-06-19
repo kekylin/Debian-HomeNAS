6a9929a72cc5eda79286d695c638a7a9152cad76
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-19 19:05:30 +0800

    Update system_init.sh

--- Shell/system_init.sh
+++ Shell/system_init.sh
@@ -2,28 +2,20 @@
 
 # 备份镜像源配置文件
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
 # 更换国内镜像源
 cat << EOF > /etc/apt/sources.list
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
-
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
-
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
-
-# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
 deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
 EOF
 
 # 更新系统
 apt update && apt upgrade -y
 
 # 安装必备软件
-apt install -y sudo curl git vim wget exim4 gnupg apt-transport-https ca-certificates
+apt install -y sudo curl git vim wget exim4 gnupg apt-transport-https ca-certificates smartmontools
 
 # 添加第一个创建的用户（ID：1000）至sudo组
 first_user=$(awk -F: '$3>=1000 && $1 != "nobody" {print $1}' /etc/passwd | sort | head -n 1)
