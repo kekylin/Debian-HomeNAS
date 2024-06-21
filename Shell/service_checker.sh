d91bbb27b36caae05f1ec8bcf39f79c83a9a3cad
kekylin <65521882+kekylin@users.noreply.github.com>
2024-06-21 21:03:58 +0800

    优化代码

--- Shell/service_checker.sh
+++ Shell/service_checker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # ANSI 转义码颜色定义
-green='\033[0;32m'
-red='\033[0;31m'
-reset='\033[0m'
+readonly GREEN='\033[0;32m'
+readonly RESET='\033[0m'
 
 # 函数：检查服务是否运行并尝试启动服务
 check_and_start_service() {
     local service_name=$1
-    local service_status=$(systemctl is-active $service_name.service 2>/dev/null)
-    if [ "$service_status" != "active" ]; then
-        sudo systemctl start $service_name.service >/dev/null 2>&1
-        service_status=$(systemctl is-active $service_name.service 2>/dev/null)
+    local service_status
+    service_status=$(systemctl is-active "$service_name.service" 2>/dev/null)
+    if [[ $service_status != "active" ]]; then
+        systemctl start "$service_name.service" >/dev/null 2>&1
+        service_status=$(systemctl is-active "$service_name.service" 2>/dev/null)
     fi
     echo "$service_status"
 }
@@ -19,8 +19,7 @@ check_and_start_service() {
 # 函数：检查 Docker 容器是否运行
 check_docker_container_running() {
     local container_name=$1
-    local running_containers=$(docker ps -q -f "name=$container_name" | wc -l)
-    if [ "$running_containers" -gt 0 ]; then
+    if docker ps -q -f "name=$container_name" | grep -q .; then
         echo "active"
     else
         echo "inactive"
@@ -31,37 +30,35 @@ check_docker_container_running() {
 print_service_status() {
     local service_name=$1
     local service_url=$2
-    local service_status=$3
-
-    if [ "$service_status" == "active" ]; then
-        echo -e "${green}$service_name 服务已运行！${reset}"
-        echo -e "请通过浏览器访问 $service_url"
-    else
-        echo -e "${red}$service_name 服务未安装或未正常运行，请检查。${reset}"
-    fi
+    echo -e "${GREEN}$service_name 服务已运行！${RESET}"
+    echo -e "请通过浏览器访问 $service_url"
 }
 
 # 获取主机的 IP 地址
 host_ip=$(hostname -I | awk '{print $1}')
 
 # Cockpit Web 服务
-cockpit_status=$(check_and_start_service cockpit)
-print_service_status "Cockpit Web" "https://$host_ip:9090" "$cockpit_status"
+if [[ $(check_and_start_service cockpit) == "active" ]]; then
+    print_service_status "Cockpit Web" "https://$host_ip:9090"
+fi
 
 # Docker 服务
-docker_status=$(systemctl is-active docker.service 2>/dev/null)
-if [ "$docker_status" != "active" ]; then
-    echo -e "${red}Docker 服务未安装或未正常运行，请检查。${reset}"
-fi
+if [[ $(check_and_start_service docker) == "active" ]]; then
+    # 获取本机IP地址
+    host_ip=$(hostname -I | awk '{print $1}')
 
-# Portainer 服务
-if [ "$docker_status" == "active" ]; then
-    portainer_status=$(check_docker_container_running "portainer")
-    print_service_status "Portainer" "https://$host_ip:9443" "$portainer_status"
-fi
-
-# Dockge 服务
-if [ "$docker_status" == "active" ]; then
-    dockge_status=$(check_docker_container_running "dockge")
-    print_service_status "Dockge" "http://$host_ip:5001" "$dockge_status"
+    # 定义要检查的Docker容器名称及其访问地址
+    declare -A docker_containers=(
+        ["dockge"]="http://$host_ip:5001"
+        ["nginx-ui"]="http://$host_ip:12800"
+        ["portainer"]="https://$host_ip:9443"
+        ["portainer_zh-cn"]="http://$host_ip:9999"
+        ["scrutiny"]="http://$host_ip:9626"
+    )
+    
+    for container_name in "${!docker_containers[@]}"; do
+        if [[ $(check_docker_container_running "$container_name") == "active" ]]; then
+            print_service_status "$container_name" "${docker_containers[$container_name]}"
+        fi
+    done
 fi
