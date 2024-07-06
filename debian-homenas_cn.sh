e6e9acca3bfb89442418ef4ad191faec185b435d
kekylin <65521882+kekylin@users.noreply.github.com>
2024-07-06 20:46:59 +0800

    Update debian-homenas_cn.sh

--- debian-homenas_cn.sh
+++ debian-homenas_cn.sh
@@ -65,7 +65,7 @@ show_welcome() {
     color_print $COLOR_BLUE "=================================================="
     echo -e "                 Debian HomeNAS\n\n                                  QQ群：339169752\n作者：kekylin\n项目：https://github.com/kekylin/Debian-HomeNAS"
     if [ "$first_run" = true ]; then
-        color_print $COLOR_GREEN "--------------------------------------------------\n温馨提示！\n1、系统安装后首次运行，建议执行“一键配置HomeNAS”。\n2、安装防火墙后重启一次系统再使用。\n3、一级和二级菜单都支持多选，空格分隔（如：1 3 5）。"
+        color_print $COLOR_GREEN "--------------------------------------------------\n温馨提示！\n1、系统安装后首次运行，建议执行“一键配置HomeNAS”。\n2、安装防火墙后重启一次系统再使用。\n3、菜单选项支持多选，空格分隔（如：1 3 5）。"
     fi
 }
 
