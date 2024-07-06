f33742c20aa3916da45e59a8c33def5ee5fe55bd
kekylin <65521882+kekylin@users.noreply.github.com>
2024-07-06 20:18:49 +0800

    Update deploy-containers.sh

--- Shell/deploy-containers.sh
+++ Shell/deploy-containers.sh
@@ -43,7 +43,7 @@ install_container() {
 # 显示菜单并处理用户输入（按照容器名称排序）
 show_menu() {
     local choices=()
-    echo "菜单："
+    echo "可安装容器应用："
     for name in "${!containers[@]}"; do
         choices+=("$name")
     done
