a98af80492a89110c9e29153d12a4e2a660733a5
kekylin <65521882+kekylin@users.noreply.github.com>
2024-07-16 20:52:55 +0800

    Update system_security.sh

--- Shell/system_security.sh
+++ Shell/system_security.sh
@@ -51,10 +51,64 @@ EOF
     fi
 }
 
+# 3. 用户登录系统发送邮件告警
+function configure_login_notification {
+    read -p "是否设置用户登录系统发送邮件告警？(y/n): " choice
+    if [ "$choice" == "y" ]; then
+        read -p "请输入邮箱地址: " email
+        # 检查是否已存在/etc/pam.d/login-notifiy.sh配置文件
+        if [ -f "/etc/pam.d/login-notifiy.sh" ]; then
+            # 清空文件内容
+            echo "" > /etc/pam.d/login-notifiy.sh
+        else
+            # 如果文件不存在，则新建
+            touch /etc/pam.d/login-notifiy.sh
+        fi
+        # 插入脚本内容
+        cat << EOF > /etc/pam.d/login-notifiy.sh
+#!/bin/bash
+
+export LANG="en_US.UTF-8"
+
+[ "\$PAM_TYPE" = "open_session" ] || exit 0
+{
+echo "用户: \$PAM_USER"
+echo "远程用户: \$PAM_RUSER"
+echo "远程主机: \$PAM_RHOST"
+echo "服务: \$PAM_SERVICE"
+echo "终端: \$PAM_TTY"
+echo "日期: \`date '+%Y年%m月%d日%H时%M分%S秒'\`"
+echo "服务器: \`uname -s -n -r\`"
+} | mail -s "注意! 用户\$PAM_USER正通过\$PAM_SERVICE服务登录\`hostname -s | awk '{print toupper(substr(\$0,1,1)) substr(\$0,2)}'\`系统" $email
+EOF
+        echo "已配置用户登录系统发送邮件告警。"
+        # 修改权限
+        chmod +x /etc/pam.d/login-notifiy.sh
+
+        # 检查是否已经配置了对应的参数
+        if grep -q "login-notifiy.sh" /etc/pam.d/common-session; then
+            echo "已配置用户登录系统发送邮件告警，跳过配置。"
+        else
+            # 在/etc/pam.d/common-session配置文件末行追加内容
+            echo "session optional pam_exec.so debug /bin/bash /etc/pam.d/login-notifiy.sh" >> /etc/pam.d/common-session
+            echo "已添加用户登录系统发送邮件告警。"
+        fi
+    else
+        # 如果用户选择不设置通知，则检查并删除已有的配置
+        if grep -q "login-notifiy.sh" /etc/pam.d/common-session; then
+            sed -i '/login-notifiy.sh/d' /etc/pam.d/common-session
+            echo "已删除用户登录系统发送邮件告警配置。"
+        else
+            echo "未设置用户登录系统发送邮件告警，跳过配置。"
+        fi
+    fi
+}
+
 # 主函数
 function main {
     configure_su_restrictions
     configure_timeout_and_logging
+    configure_login_notification
 }
 
 # 执行主函数
