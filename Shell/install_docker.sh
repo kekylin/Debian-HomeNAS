273caaf2a126f3230e50cb593d7e02a55d3f9f7a
kekylin <65521882+kekylin@users.noreply.github.com>
2024-07-18 18:02:13 +0800

    更改docker安装源为清华镜像源

--- Shell/install_docker.sh
+++ Shell/install_docker.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+# 使用清华镜像源安装Docker
 
-# 使用华为镜像源安装Docker
 # 安装依赖
+apt-get update
 apt-get install ca-certificates curl gnupg
+
 # 信任 Docker 的 GPG 公钥并添加仓库
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://mirrors.huaweicloud.com/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.huaweicloud.com/docker-ce/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 # 更新索引文件并安装 Docker 相关组件
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
