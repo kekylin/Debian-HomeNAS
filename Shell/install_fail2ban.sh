415884b828f5757b094c375c00a831d4a6a60440
kekylin <65521882+kekylin@users.noreply.github.com>
2024-08-14 12:27:54 +0800

    Update install_fail2ban.sh

--- Shell/install_fail2ban.sh
+++ Shell/install_fail2ban.sh
@@ -66,7 +66,17 @@ norestored = 1
 actionstart = printf %%b "你好！\n监视到【<name>】服务已成功启动。\n敬请注意！\nFail2Ban"|mail -s "[Fail2Ban] <name>: 在 <fq-hostname> 服务器上启动" <dest>
 actionstop = printf %%b "你好！\n监视到【<name>】服务已被停止。\n敬请注意！\nFail2Ban"|mail -s "[Fail2Ban] <name>: 在 <fq-hostname> 服务器上停止" <dest>
 actioncheck =
-actionban = printf %%b "警告!!!\n攻击者IP：<ip>\n被攻击机器名：$(uname -n) \n被攻击机器IP：$(/bin/curl ifconfig.co) \n攻击服务：<name> \n攻击次数：<failures> 次 \n攻击方法：暴力破解，尝试弱口令.\n该IP：<ip>已经被Fail2Ban加入防火墙黑名单,屏蔽时间<bantime>秒.\n\n以下是攻击者 <ip>信息 :\n$(/bin/curl https://api.vore.top/api/IPdata?ip=\$ip)\n\nFail2Ban邮件提醒\n\n "|/bin/mailx -s "<fq-hostname>服务器:<name>服务疑似遭到<ip>暴力攻击。" <dest>
+actionban = printf %%b "警告!!!\n
+            攻击者IP：<ip>\n
+            被攻击机器名：`uname -n` \n
+            被攻击机器IP：`/bin/curl ifconfig.co` \n
+            攻击服务：<name> \n
+            攻击次数：<failures> 次 \n
+            攻击方法：暴力破解，尝试弱口令.\n
+            该IP：<ip>已经被Fail2Ban加入防火墙黑名单,屏蔽时间<bantime>秒.\n\n
+            以下是攻击者 <ip>信息 :\n
+            `/bin/curl https://api.vore.top/api/IPdata?ip=<ip>`\n\n
+            Fail2Ban邮件提醒\n\n "|/bin/mailx -s "<fq-hostname>服务器:<name>服务疑似遭到<ip>暴力攻击。" <dest>
 actionunban =
 [Init]
 name = default
