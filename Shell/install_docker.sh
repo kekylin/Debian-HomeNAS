bf0778762332103164d697cdc58c044d8dc28b91
kekylin <65521882+kekylin@users.noreply.github.com>
2024-08-14 17:22:30 +0800

    Docker公钥下载地址从华为源改为清华源

--- Shell/install_docker.sh
+++ Shell/install_docker.sh
@@ -7,7 +7,7 @@ apt-get install ca-certificates curl gnupg
 
 # 信任 Docker 的 GPG 公钥并添加仓库
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://mirrors.huaweicloud.com/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian \
